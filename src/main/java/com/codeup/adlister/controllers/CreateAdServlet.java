5f2f6002cf4d7d39de08d61470b2e2b06e32720f
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-07-27 14:18:22 -0700

    Created Servlet for search, Added checkboxes for categories to create page. Added handling of catagories to Create servlet. Added set catagories method to MySQLAdsDao

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.MySQLAdsDao;
 import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
@@ -32,7 +35,19 @@ public class CreateAdServlet extends HttpServlet {
             request.getParameter("title"),
             request.getParameter("description")
         );
-        DaoFactory.getAdsDao().insert(ad);
+        long index = DaoFactory.getAdsDao().insert(ad);
+        String[] categories = request.getParameterValues("categories");
+
+        if (categories != null) {
+            for (String category : categories) {
+                try {
+                    DaoFactory.getAdsDao().setCategories(index, category);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
         response.sendRedirect("/ads");
     }
 }
--- src/main/webapp/WEB-INF/ads/create.jsp
+++ src/main/webapp/WEB-INF/ads/create.jsp
@@ -8,7 +8,7 @@
 <body>
     <div class="container">
         <h1>Create a new Ad</h1>
-        <form action="/ads/create" method="post">
+        <form action="/ads/create" method="POST">
             <div class="form-group">
                 <label for="title">Title</label>
                 <input id="title" name="title" class="form-control" type="text">
@@ -16,6 +16,14 @@
             <div class="form-group">
                 <label for="description">Description</label>
                 <textarea id="description" name="description" class="form-control" type="text"></textarea>
+            </div>
+            <div>
+                <input type="checkbox" id="cat1" name="categories" value="cat1">
+                <label for="cat1">Cat 1</label>
+                <input type="checkbox" id="cat2" name="categories" value="cat2">
+                <label for="cat2">Cat 2</label>
+                <input type="checkbox" id="cat3" name="categories" value="cat3">
+                <label for="cat3">Cat 3</label>
             </div>
             <input type="submit" class="btn btn-block btn-primary">
         </form>
